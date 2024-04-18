a9a45772ac2198e642c8287578a72ba39632d630
dongkyung <kdkd422@naver.com>
2024-04-18 17:59:10 +0900

    Typo

---
+++ presentation/src/main/java/com/dkproject/presentation/ui/theme/Type.kt
@@ -0,0 +1,47 @@
+package com.dkproject.presentation.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dkproject.presentation.R
+
+val ubuntu = FontFamily(
+    Font(R.font.ubuntu_bolditalic)
+)
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 65.sp,
+        letterSpacing = 1.2.sp,
+        fontWeight = FontWeight.Bold
+    )
+
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)
