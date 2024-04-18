3a74f14fdd468c951fe61eda3d57d30a675e531b
dongkyung <kdkd422@naver.com>
2024-04-18 17:52:06 +0900

    메인 홈 스크린

---
+++ presentation/src/main/java/com/dkproject/presentation/ui/screen/home/HomeScreen.kt
@@ -0,0 +1,14 @@
+package com.dkproject.presentation.ui.screen.home
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun HomeScreen() {
+    Surface(modifier=Modifier.fillMaxSize()) {
+
+    }
+}
