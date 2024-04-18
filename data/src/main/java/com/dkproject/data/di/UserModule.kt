4905fa809f4e1f8ec8bb1c400577b28cf94cc95d
dongkyung <kdkd422@naver.com>
2024-04-18 18:01:25 +0900

    Hilt - UserModule

---
+++ data/src/main/java/com/dkproject/data/di/UserModule.kt
@@ -0,0 +1,62 @@
+package com.dkproject.data.di
+
+
+import com.dkproject.data.datastore.UserDataStore
+import com.dkproject.data.repository.token.TokenRepositoryImpl
+import com.dkproject.data.usecase.login.LoginUseCaseImpl
+import com.dkproject.data.usecase.signup.SignUpUseCaseImpl
+import com.dkproject.data.usecase.user.GetUserInfoUseCaseImpl
+import com.dkproject.data.usecase.user.UpdateMyInfoUseCaseImpl
+import com.dkproject.domain.repository.TokenRepository
+import com.dkproject.domain.usecase.login.LoginUseCase
+import com.dkproject.domain.usecase.signup.SignUpUseCase
+import com.dkproject.domain.usecase.token.ClearTokenUseCase
+import com.dkproject.domain.usecase.token.GetTokenUseCase
+import com.dkproject.domain.usecase.token.SetTokenUseCase
+import com.dkproject.domain.usecase.user.GetUserInfoUseCase
+import com.dkproject.domain.usecase.user.UpdateMyInfoUseCase
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object  UserModule {
+    @Provides
+    fun providesGetTokenUseCase(tokenRepository: TokenRepository):GetTokenUseCase{
+        return GetTokenUseCase(tokenRepository)
+    }
+
+    @Provides
+    fun providesSetTokenUseCase(tokenRepository: TokenRepository):SetTokenUseCase{
+        return SetTokenUseCase(tokenRepository)
+    }
+
+    @Provides
+    fun providesClearTokenUseCase(tokenRepository: TokenRepository):ClearTokenUseCase{
+        return ClearTokenUseCase(tokenRepository)
+    }
+
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class BindUserMoudle(){
+    @Binds
+    abstract fun bindTokenRepository(uc:TokenRepositoryImpl):TokenRepository
+
+    @Binds
+    abstract fun bindLoginUseCase(uc:LoginUseCaseImpl):LoginUseCase
+
+    @Binds
+    abstract fun bindSingUpUseCase(uc:SignUpUseCaseImpl):SignUpUseCase
+
+    @Binds
+    abstract fun bindUserInfoUpUseCase(uc:GetUserInfoUseCaseImpl):GetUserInfoUseCase
+
+    @Binds
+    abstract fun bindUpdateUserInfoUpUseCase(uc:UpdateMyInfoUseCaseImpl):UpdateMyInfoUseCase
+}
