37ea9b1991ff03ac01ce4d8b95d71f8ce14bd361
dongkyung <kdkd422@naver.com>
2024-04-18 18:01:37 +0900

    Retrofit - UserService

---
+++ data/src/main/java/com/dkproject/data/retrofit/UserService.kt
@@ -0,0 +1,32 @@
+package com.dkproject.data.retrofit
+
+import com.dkproject.data.model.dto.UserDTO
+import com.dkproject.domain.model.CommonResponse
+import okhttp3.RequestBody
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+
+interface UserService {
+
+    @POST("api/users/login")
+    suspend fun login(
+        @Body requestBody: RequestBody
+    ): CommonResponse<String>
+
+    @POST("api/users/sign-up")
+    suspend fun signUp(
+        @Body requestBody: RequestBody
+    ): CommonResponse<Long>
+
+    @GET("api/users/my-page")
+    suspend fun myPage():CommonResponse<UserDTO>
+
+    @PATCH("api/users/my-page")
+    suspend fun patchMyPage(
+        @Body requestBody: RequestBody
+    ):CommonResponse<Long>
+
+}
