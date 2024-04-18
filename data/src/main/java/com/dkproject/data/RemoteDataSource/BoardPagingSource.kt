1d3855cabff8bbc36cdaddfdee6124cce2a4c8c5
dongkyung <kdkd422@naver.com>
2024-04-18 17:42:46 +0900

    게시판 페이징 소스

---
+++ data/src/main/java/com/dkproject/data/RemoteDataSource/BoardPagingSource.kt
@@ -0,0 +1,42 @@
+package com.dkproject.data.RemoteDataSource
+
+import android.util.Log
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.dkproject.domain.model.Board
+import javax.inject.Inject
+
+class BoardPagingSource @Inject constructor(
+    private val boardDataSource: BoardDataSource
+) : PagingSource<Int, Board>() {
+    override fun getRefreshKey(state: PagingState<Int, Board>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Board> {
+        try {
+            val page:Int = params.key?:1
+            val loadSize = params.loadSize
+            var setdata :MutableList<Board> =  mutableListOf()
+            val data = boardDataSource.getBoard(page = page, size = loadSize).onSuccess {
+                Log.d("BoardPagingSource", "sucess")
+                setdata=it.toMutableList()
+            }.onFailure {
+                Log.d("BoardPagingSource", "errorfail")
+            }
+            Log.d("BoardPagingSource", data.toString())
+            val size = setdata.size
+            return LoadResult.Page(
+                data = setdata,
+                prevKey = null,
+                nextKey = if (size == params.loadSize) page + 1 else null
+            )
+        } catch (e: Exception) {
+            Log.d("BoardPagingSource", "error")
+            return LoadResult.Error(e)
+        }
+    }
+}
