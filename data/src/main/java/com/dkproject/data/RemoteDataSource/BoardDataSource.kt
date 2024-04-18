55a26d8ed2ca90e135ea74e7715325bbd3b901ae
dongkyung <kdkd422@naver.com>
2024-04-18 17:42:21 +0900

    게시판 데이터 소스

---
+++ data/src/main/java/com/dkproject/data/RemoteDataSource/BoardDataSource.kt
@@ -0,0 +1,19 @@
+package com.dkproject.data.RemoteDataSource
+
+import com.dkproject.data.model.dto.BoardDTO
+import com.dkproject.data.retrofit.BoardService
+import com.dkproject.domain.model.Board
+import com.dkproject.domain.model.CommonResponse
+import javax.inject.Inject
+
+interface BoardDataSource {
+    suspend fun getBoard(page:Int,size:Int):Result<List<Board>>
+}
+
+class BoardRemoteDataSource @Inject constructor(
+    private val boardService: BoardService
+):BoardDataSource {
+    override suspend fun getBoard(page: Int, size: Int): Result<List<Board>> = runCatching {
+        boardService.getBoard(page=page,size=size).data.map { it.toDomainModel() }
+    }
+}
