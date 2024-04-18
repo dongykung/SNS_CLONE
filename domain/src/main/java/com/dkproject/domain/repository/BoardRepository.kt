4a0d8ad7f8935e690933f985b1560911b1120936
dongkyung <kdkd422@naver.com>
2024-04-18 17:43:51 +0900

    게시판 저장소

---
+++ domain/src/main/java/com/dkproject/domain/repository/BoardRepository.kt
@@ -0,0 +1,9 @@
+package com.dkproject.domain.repository
+
+import androidx.paging.PagingData
+import com.dkproject.domain.model.Board
+import kotlinx.coroutines.flow.Flow
+
+interface BoardRepository {
+    suspend operator fun invoke():Result<Flow<PagingData<Board>>>
+}
