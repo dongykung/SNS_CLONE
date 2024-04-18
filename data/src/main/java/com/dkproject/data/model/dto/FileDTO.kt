912877a4f524f221347bad6b599aee83eb5d03db
dongkyung <kdkd422@naver.com>
2024-04-18 17:48:54 +0900

    파일 업로드 DTO

---
+++ data/src/main/java/com/dkproject/data/model/dto/FileDTO.kt
@@ -0,0 +1,11 @@
+package com.dkproject.data.model.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FileDTO(
+    val id:Long,
+    val fileName:String,
+    val createdAt:String,
+    val filePath:String,
+)
