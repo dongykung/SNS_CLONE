824699ee7c847e00f285b671536c70a212677869
dongkyung <kdkd422@naver.com>
2024-04-22 23:31:42 +0900

    보드 Ui 변경

--- presentation/src/main/java/com/dkproject/presentation/ui/screen/board/BoardScreen.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/screen/board/BoardScreen.kt
@@ -1,5 +1,8 @@
 package com.dkproject.presentation.ui.screen.board
 
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -26,39 +29,51 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.dkproject.domain.model.Comment
 import com.dkproject.presentation.model.BoardCardModel
+import com.dkproject.presentation.ui.components.CommentDialog
 import com.dkproject.presentation.ui.components.CustomBottomSheet
 import com.dkproject.presentation.ui.components.CustomImage
 import com.dkproject.presentation.ui.components.CustomTopAppBar
 import com.dkproject.presentation.ui.screen.writing.ImagePager
 import com.dkproject.presentation.ui.theme.SNS_CloneTheme
+import com.dkproject.presentation.util.formatElapsedTime
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BoardScreen(
-    modifier: Modifier=Modifier,
-    viewModel: BoardViewModel
+    modifier: Modifier = Modifier,
+    viewModel: BoardViewModel,
+    onClickUser: (Long) -> Unit
 ) {
     val state = viewModel.state.collectAsState().value
     val items: LazyPagingItems<BoardCardModel> = state.boardItems.collectAsLazyPagingItems()
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Log.d("BoardScreen", items.toString())
+
+    val scrollBehavior =
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     var modelDialog: BoardCardModel? by remember { mutableStateOf(null) }
 
     CustomBottomSheet(model = modelDialog, dismiss = { modelDialog = null }, onBoardDelte = {
@@ -73,7 +88,8 @@ fun BoardScreen(
                 CustomTopAppBar(title = "SNS", scrollBehavior = scrollBehavior)
             }) { innerPadding ->
             Column(modifier = Modifier.padding(innerPadding)) {
-                LazyColumn(modifier = Modifier.fillMaxSize(),
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
                     contentPadding = PaddingValues(6.dp)
                 ) {
                     items(count = items.itemCount,
@@ -83,16 +99,17 @@ fun BoardScreen(
                         items[index]?.run {
                             if (!state.deletedBoardItems.contains(this.boardId))
                                 BoardCard(
+                                    userId = this.userId,
                                     profileImageUrl = this.userProfileUrl,
                                     username = this.username,
                                     images = this.images,
+                                    createdAt = this.createAt,
                                     text = this.text,
-                                    onReplyClick = {
-
-                                    },
+                                    comments = this.commentList,
                                     onOptionClick = {
                                         modelDialog = this
-                                    })
+                                    },
+                                    onClickUser = onClickUser)
                         }
                     }
 
@@ -105,28 +122,40 @@ fun BoardScreen(
 
 @Composable
 fun BoardCard(
+    userId: Long,
     profileImageUrl: String,
     username: String,
     images: List<String>,
     text: String,
+    createdAt: String,
+    comments: List<Comment>,
+    onClickUser:(Long)->Unit,
     onOptionClick: () -> Unit,
-    onReplyClick: () -> Unit,
 ) {
+    Log.d("BoardCard", userId.toString())
+    var commentDialogVisible by remember {
+        mutableStateOf(false)
+    }
     Column {
         //헤더
         BoarderHeader(
             profileImageUrl = profileImageUrl,
             username = username,
-            onOptionClick = onOptionClick
+            createdAt = createdAt,
+            userId = userId,
+            onOptionClick = onOptionClick,
+            onClickUser = onClickUser
         )
         //이미지 페이저
-        ImagePager(modifier = Modifier
-            .fillMaxWidth()
-            .height(300.dp), imageList = images)
+        ImagePager(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp), imageList = images
+        )
 
         // 내용
-        var maxLines by remember { mutableStateOf(1) }
-        var showMore by remember { mutableStateOf(false) }
+        var maxLines by rememberSaveable { mutableStateOf(1) }
+        var showMore by rememberSaveable { mutableStateOf(false) }
         Text(
             modifier = Modifier
                 .fillMaxWidth()
@@ -156,19 +185,26 @@ fun BoardCard(
                 .padding(top = 8.dp)
                 .padding(horizontal = 8.dp)
                 .align(Alignment.End),
-            onClick = onReplyClick
+            onClick = { commentDialogVisible = true }
         ) {
             Text(text = "댓글")
         }
     }
-
+    CommentDialog(visible = commentDialogVisible,
+        comments = comments,
+        onDismiss = { commentDialogVisible = false },
+        onCloseClick = { commentDialogVisible = false },
+        onClickUser = onClickUser)
 }
 
 @Composable
 fun BoarderHeader(
     modifier: Modifier = Modifier,
+    userId:Long,
     profileImageUrl: String,
+    createdAt:String,
     username: String,
+    onClickUser: (Long) -> Unit,
     onOptionClick: () -> Unit,
 ) {
     Row(
@@ -181,16 +217,23 @@ fun BoarderHeader(
         //profile Image
         CustomImage(
             modifier = Modifier
-                .size(36.dp)
+                .clickable { onClickUser(userId) }
+                .size(38.dp)
                 .clip(CircleShape), url = profileImageUrl
         )
         //userName
-        Text(
-            modifier = Modifier.padding(horizontal = 8.dp),
-            text = username,
-            style = MaterialTheme.typography.headlineSmall,
-            color = Color.White
-        )
+        Column(verticalArrangement = Arrangement.Center) {
+            Text(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                text = username,
+                style  = TextStyle(color=Color.White, fontSize = 18.sp,fontWeight = FontWeight.Bold),
+                color = Color.White
+            )
+            Text(modifier = Modifier.padding(horizontal = 8.dp),
+                text = formatElapsedTime(createdAt),
+                style = TextStyle(color=Color.White, fontSize = 14.sp)
+            )
+        }
         Spacer(modifier = Modifier.weight(1f))
         IconButton(onClick = { onOptionClick() }) {
             Icon(
@@ -202,16 +245,3 @@ fun BoarderHeader(
     }
 }
 
-@Composable
-@Preview
-fun testpreview() {
-    SNS_CloneTheme {
-        BoardCard(
-            profileImageUrl = "asdf",
-            username = "dongkyung",
-            images = emptyList(),
-            text = "hello",
-            onOptionClick = { },
-            onReplyClick = {})
-    }
-}
