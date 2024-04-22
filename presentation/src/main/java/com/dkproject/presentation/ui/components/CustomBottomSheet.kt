0b424d4647a2e049ee88fd74688b3e7571ee4fc3
dongkyung <kdkd422@naver.com>
2024-04-22 23:32:38 +0900

    바텀시트 커스텀

--- presentation/src/main/java/com/dkproject/presentation/ui/components/CustomBottomSheet.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/components/CustomBottomSheet.kt
@@ -1,11 +1,13 @@
 package com.dkproject.presentation.ui.components
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.dkproject.presentation.model.BoardCardModel
 
 
@@ -16,9 +18,10 @@ fun CustomBottomSheet(
     dismiss: () -> Unit,
     onBoardDelte: (BoardCardModel) -> Unit,
 ) {
-    val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = false)
     model?.run {
         ModalBottomSheet(
+            modifier=Modifier.fillMaxSize(),
             sheetState = bottomSheetState,
             onDismissRequest = dismiss
         ) {
