d3f1da65870f95d1ce2ac4e2a4befe63f8b74b2a
dongkyung <kdkd422@naver.com>
2024-04-23 07:52:46 +0900

    유저 의존성 모듈

--- data/src/main/java/com/dkproject/data/di/UserModule.kt
+++ data/src/main/java/com/dkproject/data/di/UserModule.kt
@@ -1,18 +1,23 @@
 package com.dkproject.data.di
 
 
-import com.dkproject.data.datastore.UserDataStore
+import com.dkproject.data.RemoteDataSource.UserDataSource
+import com.dkproject.data.RemoteDataSource.UserDataSourceImpl
 import com.dkproject.data.repository.token.TokenRepositoryImpl
+import com.dkproject.data.repository.user.UserRepositoryImpl
 import com.dkproject.data.usecase.login.LoginUseCaseImpl
 import com.dkproject.data.usecase.signup.SignUpUseCaseImpl
+import com.dkproject.data.usecase.user.GetMyInfoUseCaseImpl
 import com.dkproject.data.usecase.user.GetUserInfoUseCaseImpl
 import com.dkproject.data.usecase.user.UpdateMyInfoUseCaseImpl
 import com.dkproject.domain.repository.TokenRepository
+import com.dkproject.domain.repository.UserRepository
 import com.dkproject.domain.usecase.login.LoginUseCase
 import com.dkproject.domain.usecase.signup.SignUpUseCase
 import com.dkproject.domain.usecase.token.ClearTokenUseCase
 import com.dkproject.domain.usecase.token.GetTokenUseCase
 import com.dkproject.domain.usecase.token.SetTokenUseCase
+import com.dkproject.domain.usecase.user.GetMyInfoUseCase
 import com.dkproject.domain.usecase.user.GetUserInfoUseCase
 import com.dkproject.domain.usecase.user.UpdateMyInfoUseCase
 import dagger.Binds
@@ -55,8 +60,17 @@ abstract class BindUserMoudle(){
     abstract fun bindSingUpUseCase(uc:SignUpUseCaseImpl):SignUpUseCase
 
     @Binds
-    abstract fun bindUserInfoUpUseCase(uc:GetUserInfoUseCaseImpl):GetUserInfoUseCase
+    abstract fun bindUserInfoUpUseCase(uc:GetMyInfoUseCaseImpl):GetMyInfoUseCase
 
     @Binds
     abstract fun bindUpdateUserInfoUpUseCase(uc:UpdateMyInfoUseCaseImpl):UpdateMyInfoUseCase
+
+    @Binds
+    abstract fun bindUserDataSource(uc:UserDataSourceImpl):UserDataSource
+
+    @Binds
+    abstract fun bindUserRepository(uc:UserRepositoryImpl):UserRepository
+
+    @Binds
+    abstract fun bindGetUserInfoUseCase(uc : GetUserInfoUseCaseImpl):GetUserInfoUseCase
 }
