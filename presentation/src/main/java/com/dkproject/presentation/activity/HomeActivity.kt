8ef49c33692d0e955e8d98cc507c999f4829c132
dongkyung <kdkd422@naver.com>
2024-04-24 16:06:59 +0900

    홈 액티비티 = 뷰모델 관리

--- presentation/src/main/java/com/dkproject/presentation/activity/HomeActivity.kt
+++ presentation/src/main/java/com/dkproject/presentation/activity/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -12,12 +13,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import com.dkproject.domain.model.ACTION_POSTED
 import com.dkproject.presentation.navigation.HomeNavigation
 import com.dkproject.presentation.ui.screen.board.BoardViewModel
 import com.dkproject.presentation.ui.screen.setting.SettingViewModel
 import com.dkproject.presentation.ui.theme.SNS_CloneTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.log
 
 @AndroidEntryPoint
 class HomeActivity : ComponentActivity() {
@@ -39,7 +42,13 @@ class HomeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SNS_CloneTheme {
-                HomeNavigation(boardViewModel,settingViewModel)
+                HomeNavigation(boardViewModel,settingViewModel, onBoardDelete = {id->
+                    boardViewModel.onBoardDelete(id, deleteSuccess = {
+                        Log.d("deletetest","dete")
+                        settingViewModel.onBoardDelete(id)
+                    })
+
+                })
             }
         }
         ContextCompat.registerReceiver(
