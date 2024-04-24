28e7721adabe5ab94cc5545bb01eeca4b5b8ef8d
dongkyung <kdkd422@naver.com>
2024-04-24 16:05:01 +0900

    댓글 유즈케이스

--- data/src/main/java/com/dkproject/data/di/BoardModule.kt
+++ data/src/main/java/com/dkproject/data/di/BoardModule.kt
@@ -4,12 +4,16 @@ import com.dkproject.data.RemoteDataSource.BoardDataSource
 import com.dkproject.data.RemoteDataSource.BoardRemoteDataSource
 import com.dkproject.data.repository.board.BoardRepositoryImpl
 import com.dkproject.data.usecase.board.DeleteBoardUseCaseImpl
+import com.dkproject.data.usecase.board.DeleteCommentUseCaseImpl
 import com.dkproject.data.usecase.board.GetBoardListUseCaseImpl
 import com.dkproject.data.usecase.board.GetMyBoardListUseCaseImpl
+import com.dkproject.data.usecase.board.PostCommentUseCaseImpl
 import com.dkproject.domain.repository.BoardRepository
 import com.dkproject.domain.usecase.board.DeleteBoardUseCase
+import com.dkproject.domain.usecase.board.DeleteCommentUseCase
 import com.dkproject.domain.usecase.board.GetBoardListUseCase
 import com.dkproject.domain.usecase.board.GetMyBoardUseCase
+import com.dkproject.domain.usecase.board.PostCommentUseCase
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -34,4 +38,10 @@ abstract class BoardModule {
 
     @Binds
     abstract fun bindGetMYBoarddUseCase(uc:GetMyBoardListUseCaseImpl):GetMyBoardUseCase
+
+    @Binds
+    abstract fun bindCommentUseCase(uc:PostCommentUseCaseImpl):PostCommentUseCase
+
+    @Binds
+    abstract fun bindDeleteCommentUseCase(uc:DeleteCommentUseCaseImpl):DeleteCommentUseCase
 }
